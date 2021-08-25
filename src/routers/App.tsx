e1ba687db9eaaf250affda8ee2040e8b007f9143
guochao <463561248@qq.com>
2021-08-26 00:44:55 +0800

    feat: ts目录的引入

--- src/routers/App.tsx
+++ src/routers/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Text, View } from 'react-native';
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <View>
+      <Text>App</Text>
+    </View>
   );
 }
 
