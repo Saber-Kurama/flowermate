22666cb8f42e5bad1220b94694f6cde2d6b64976
guochao <463561248@qq.com>
2021-09-01 01:31:45 +0800

    添加@reduxjs/toolkit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './src/reduxState/store';
 
 export default function App() {
   return (
-    <Provider store={}>
+    <Provider store={store}>
       <View>
         <Text>app 文本</Text>
       </View>
